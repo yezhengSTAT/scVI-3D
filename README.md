f961a4a0c512170118965469804702e7d67fefc3
yezhengSTAT <yezhengSTAT@gmail.com>
2021-02-22 12:09:29 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,168 @@
+# 3DVI: Normalization and De-noising of single-cell Hi-C Data
+
+Ye Zheng, Siqi Shen and Sündüz Keleş. Normalization and De-noising of single-cell Hi-C Data with BandNorm and 3DVI. bioRxiv (2021).
+
+
+## What is 3DVI?
+
+The advent of single-cell sequencing technologies in profiling 3D genome organization led to the development of single-cell high-throughput chromatin conformation (scHi-C) assays. To explicitly capture chromatin conformation features and distinguish cells based on their 3D genome organizations, we developed a fast band normalization approach, [BandNorm](https://github.com/sshen82/BandNorm), as well as a deep generative modeling framework, 3DVI, for more structured modeling of scHi-C data. Systematic benchmarking and comparisons to existing scHi-C analysis methods on a compendium of datasets with known cell types indicated that BandNorm can be as or even more effective than more structured modeling of scHi-C data for cell-type identification.  In benchmarking experiments with respect to downstream analysis of scHi-C data, BandNorm resulted in better identification of interaction domains compared to other methods. In contrast, 3DVI intrinsic de-noising provided additional power for rare cell types with respect to 3D genome structure recovery, including the A/B compartment, topologically associating domains, and detection of  significant interacting locus-pairs. Further investigations revealed that existing scHi-C models propagate the batch effects from their low-dimensional embeddings to the resulting imputation matrices. BandNorm, coupled with the Harmony method, exhibited robustness to sequencing depth and sparsity biases and was least impacted by the batch effects. 3DVI incorporates the batch effect into its deep generative model, and eliminates the batch effects from both the embeddings and de-noised realizations. Computational speeds of BandNorm and 3DVI  also present an advantage over current existing scHi-C methods.
+
+
+Current version: 0.1
+
+![3DVI Model](./figures/model.png)
+
+
+
+
+## 3DVI Usage
+
+### 1. Preparation
+
+    git clone https://github.com/yezhengSTAT/3DVI
+
+3DVI installation is finished once you successsfully git clone the repository. We provide a demo scHi-C data, sampled 400 cells of four cell types from [Ecker2019](https://www.nature.com/articles/s41592-019-0547-z), for test run.  The raw input data will be downloaded with this repository. In preparation for such run, you will need to have python (>=3.7) available on your server with corresponding modules required: 
+  - numpy (>= 1.13.1)
+  - scanpy (>= 1.6.0)
+  - pandas (>= 1.0.3)
+  - anndata (>= 0.7.4)
+  - [scvi-tools (>= 0.8.1)](https://docs.scvi-tools.org/en/stable/installation.html)
+  - joblib (>= 1.0.1)
+  - scikit-learn (>= 0.24.1)
+  
+If you want to get the UMAP and t-SNE visualization, you will need two more modules installed:
+  - matplotlib (>=3.3.4)
+  - umap (0.1.1)
+
+For quick python module installation, python-requirements.txt is provided in this repository. Run
+
+		pip install -r python-requirements.txt
+
+
+#### 1.1 [Alternative] Creating environment using conda.
+
+ - 1. Install [conda](https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html).
+
+ - 2. Build conda environment:
+
+```
+conda env create -f 3DVI_conda_environment.yml
+```
+
+ - 3. Active conda environment for 3DVI:
+
+```
+conda activate schic-3dvi
+```
+
+### 2. Setting the parameters in the "FreeHiC_parameters'' file
+
+    Parameters for the 3DVI runs:
+    bandMax       (-b/--bandMax)        : Maximum genomic distance to be processed, e.g. 10. Use "whole" to include all the band matrix for each chromosome. Default is "whole".
+    chromList     (-c/--chromList)      : List of chromosome to be processed separate by comma, e.g. "chr1,chr2,chrX". Use "whole" to include all chromosomes in the genome size file (args.genome). Default is "whole".
+    resolution    (-r/--resolution)     : Resolution of scHi-C data, e.g., 1000000.
+    batchRemoval  (-br/--batchRemoval)  : Indicator to remove batch or not. Default is False. Adding '-br' to turn it on.
+    nLatent       (-n/--nLatent)        : Dimension of latent space. Default is 100.
+    
+    Path to input and output folders:
+    inPath        (-i/--inPath)         : Path to the folder where input scHi-C data are saved.
+    outdir        (-o/--outdir)         : Path to output directory.
+    cellSummary   (-cs/--cellSummary)   : (Optional) Cell summary file with columns names to be "name" for scHi-C data file name including extension, "batch" for batch factor, "cell_type" for cluster or cell type label (tab separated file).
+    genomeSize    (-g/--genome)         : Path to genome size file (tab separated file).
+    
+    Other optional parameters:
+    gpuFlag       (-gpu/--gpuFlag)      : (Optional) Use GPU or not. Default is False. Adding '-gpu' to turn it on.
+    parallelCPU   (-p/--parallelCPU)    : (Optional) Number of CPUs to be used for parallel running. Default is 1 and no parallel computing is used.
+    pcaNum        (-pca/--pcaNum)       : (Optional) Number of principal components to be writen out. Default is 50.
+    umapPlot      (-up/--umapPlot)      : (Optional) Plot UMAP of latent embeddings. Default is False. Adding '-up' to turn it on.
+    tsnePlot      (-tp/--tsnePlot)      : (Optional) Plot t-SNE of latent embeddings. Default is False. Adding '-tp' to turn it on.
+    verbose       (-v/--verbose)        : (Optional) Verbose. Default is False. Adding '-v' to turn it on.
+
+
+### 3. Running 3DVI
+
+    python3.7 3DVI.py -b 'whole' -c 'whole' -r 1000000 -i "Path/to/3DVI/demoData" -o "Path/to/3DVI/results" -cs "Path/to/3DVI/supplementaryData/demoData_summary.txt" -g "Path/to/3DVI/supplementaryData/hg19.chrom.sizes" -br -n 100 -gpu -p 6 -pca 50 -up -tp -v
+
+### 4. Output from 3DVI
+
+Under output directory:
+
+- Path/to/3DVI/results
+    - latentEmbeddings/  
+      - norm3DVI_PCA50.txt
+      - norm3DVI_latentEmbeddingFull.txt
+    - norm3DVI/
+      - normalized count for each input scHi-C data with the same file name
+      - ...
+    - figures/
+      - norm3DVI_TSNE.pdf  
+      - norm3DVI_UMAP.pdf
+    
+![UMAP Visualization](/figures/umap.pdf)
+
+![t-SNE Visualization](/figures/tsne.pdf)
+
+### 5. Creating 3DVI runs with your own data
+
+#### 5.1 Input scHi-C data (tab separated file)
+
+```
+chr1    0       chr1    1000000 9
+chr1    1000000 chr1    1000000 200
+chr1    0       chr1    2000000 2
+chr1    1000000 chr1    2000000 4
+chr1    2000000 chr1    2000000 220
+chr1    1000000 chr1    3000000 1
+chr1    2000000 chr1    3000000 11
+chr1    3000000 chr1    3000000 197
+chr1    1000000 chr1    4000000 1
+chr1    2000000 chr1    4000000 2
+```
+
+### 5.2 Cell summary file (tab separated file and this file is optional depending on the removal batch effect or not)
+
+```
+name    batch   cell_type       depth   sparsity
+181218_21yr_2_B11_AD001_Astro.txt       181218_21yr     Astro   333281  12832
+181218_21yr_2_B6_AD006_Astro.txt        181218_21yr     Astro   875773  26222
+181218_21yr_2_B6_AD007_Astro.txt        181218_21yr     Astro   596688  20504
+181218_21yr_2_D1_AD006_Astro.txt        181218_21yr     Astro   627360  21513
+181218_21yr_2_D6_AD008_Astro.txt        181218_21yr     Astro   429143  14796
+181218_21yr_2_E12_AD006_Astro.txt       181218_21yr     Astro   582766  19050
+181218_21yr_2_E6_AD010_Astro.txt        181218_21yr     Astro   650588  20088
+181218_21yr_2_G7_AD004_Astro.txt        181218_21yr     Astro   613362  20120
+181218_21yr_2_H9_AD001_Astro.txt        181218_21yr     Astro   381341  15958
+181218_21yr_3_A12_AD008_Astro.txt       181218_21yr     Astro   784037  19827
+181218_21yr_3_A9_AD008_Astro.txt        181218_21yr     Astro   665270  22713
+181218_21yr_3_B9_AD007_Astro.txt        181218_21yr     Astro   1502920 32304
+181218_21yr_3_D5_AD008_Astro.txt        181218_21yr     Astro   1025140 21515
+181218_21yr_3_E12_AD010_Astro.txt       181218_21yr     Astro   779923  19526
+```
+
+### 5.3 Genome size file (tab separated file)
+
+```
+chr1    249250621
+chr2    243199373
+chr3    198022430
+chr4    191154276
+chr5    180915260
+chr6    171115067
+chr7    159138663
+chrX    155270560
+chr8    146364022
+chr9    141213431
+chr10   135534747
+chr11   135006516
+chr12   133851895
+chr13   115169878
+chr14   107349540
+chr15   102531392
+chr16   90354753
+chr17   81195210
+chr18   78077248
+chr20   63025520
+chr19   59128983
+chr22   51304566
+chr21   48129895
+```
---
+++ scripts/3DVI.py
@@ -0,0 +1,451 @@
+#!/usr/bin/env python
+
+## 3DVI
+## Author: Ye Zheng
+## Contact: yezheng.stat@gmail.com
+
+
+## Script to remove sequencing depth effect, batch effect and lower dimension project, denoising single-cell Hi-C data.
+## Feb 2021
+
+import sys
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import anndata
+import sys
+import os
+import glob
+from tqdm import tqdm
+# from multiprocessing import Pool
+from joblib import Parallel, delayed
+import gc
+from sklearn.decomposition import PCA
+import argparse
+import pickle
+
+def create_band_mat(x: np.array, count: np.array, diag: int, maxChromosomeSize: int) -> np.array:
+    bandMat = np.zeros(maxChromosomeSize - diag)
+    bandMat[x] = count
+    return bandMat
+
+
+class Process(object):
+    def __init__(self, resolution, chromSize=None):
+        self._RESOLUTION = resolution
+        self._chromSize = chromSize
+        self.df = None
+        self._lastchrom = None
+        self._chormdf = None
+    
+    def rescale(self, chrA, x, y, counts, resolution = None):
+        if resolution:
+            self._RESOLUTION = resolution
+        
+        xR = x // self._RESOLUTION
+        yR = y // self._RESOLUTION
+        self.df = pd.DataFrame({'chrA': chrA,
+                        'x': xR,
+                        'y': yR,
+                        'counts': counts})
+        self.df.loc[:,'diag'] = abs(yR - xR)
+        return True
+    
+    def band(self, chrom, diag, maxBand):
+        if self.df is None:
+            raise "Run process.rescale(chrA, binA, binY, counts, resolution) first."        
+        
+        if self._lastchrom is None or (self._lastchrom != chrom):
+            self._lastchrom = chrom
+            self._chormdf = self.df[self.df.chrA == chrom]            
+        
+        dat =  self._chormdf[self._chormdf.diag == diag]
+        mat = create_band_mat(dat.x.values, dat.counts.values, diag, maxBand)
+        return mat
+    
+    def band_all(self, chromSize, used_chroms = 'whole', used_diags = [i for i in range(1, 11)]):
+        if self.df is None:
+            raise "Run process.rescale(chrA, binA, binY, counts, resolution) first"
+        if chromSize:
+            self._chromSize = chromSize
+            
+        chrom = 'chrA'
+        diag_s = 'diag'
+                
+        cell_band = {}
+        for chromosome, chromosome_data in self.df.groupby(chrom):
+            if used_chroms != 'whole' and (chromosome not in self._chromSize or chromosome not in used_chroms):
+                continue
+            
+            bandSize = self._chromSize[chromosome] // self._RESOLUTION + 1
+            chromosome_band = {}
+            for diag, chromosome_diag in chromosome_data.groupby(diag_s):
+                if used_diags != 'whole' and diag not in used_diags:
+                    continue
+                x = chromosome_diag.x.values
+                count = chromosome_diag.counts.values
+                chromosome_band[diag] = create_band_mat(x, count, diag, bandSize)
+            cell_band[chromosome] = chromosome_band
+        return cell_band
+    
+def read_file(file):
+    df = pd.read_csv(file, sep = "\t", header = 0, names = ['chrA', 'binA', 'chrB', 'binB', 'counts'])
+    df.loc[:,'cell'] = file
+    return df
+
+def read_file_chrom(file, used_chroms):
+    dfTmp = pd.read_csv(file, sep = "\t", header = 0, names = ['chrA', 'binA', 'chrB', 'binB', 'counts'])
+    dfTmp.loc[:,'cell'] = file    
+    if used_chroms == 'whole':
+        df = dfTmp
+    else:
+        df = dfTmp[dfTmp.chrA.isin(used_chroms)]
+
+    return df
+
+def read_files(file_list, used_chroms = 'whole', cores = 8):
+
+    df_list = Parallel(n_jobs=cores)(delayed(read_file_chrom)(file, used_chroms) for file in file_list)
+    combined_df = pd.concat(df_list, ignore_index=True)
+    return combined_df
+    
+def process_cell(cell_raw: pd.DataFrame, binSizeDict: dict, 
+                 resolution: int, cell: str,
+                 used_chroms,
+                 used_diags):
+    process = Process(resolution)
+    process.rescale(cell_raw.chrA, cell_raw.binA, cell_raw.binB, cell_raw.counts)    
+    cell_band = process.band_all(binSizeDict, used_chroms=used_chroms, used_diags=used_diags)
+    return (cell_band, cell)
+
+
+
+def get_locuspair(imputeM, chromSelect, bandDist):
+    xvars = ['chrA','binA', 'chrB', 'binB', 'count', 'cellID']
+    tmp = imputeM.transpose().copy()
+    tmp.index.name = 'binA'
+    normCount = pd.melt(tmp.reset_index(), id_vars = ['binA'], var_name='cellID', value_name='count')
+    normCount.loc[:,'binA'] = normCount.binA.astype(int)
+    normCount.loc[:,'binB'] = normCount.binA + bandDist
+
+    normCount.loc[:,'chrA'] = chromSelect
+    normCount.loc[:,'chrB'] = chromSelect
+    normCount = normCount[xvars]
+    
+    return normCount
+
+
+def normalize(bandM, chromSelect, bandDist, nLatent = 100, batchFlag = False, gpuFlag = False):
+
+    #bandM = band_chrom_diag[chromSelect][bandDist] #pd.read_csv(args.infile, index_col = 0).round(0)
+    cellSelect = [i for i, val in enumerate(bandM.sum(axis = 1)>0) if val]
+    bandDepth = bandM[cellSelect,].sum(axis = 1).mean()
+    adata = sc.AnnData(bandM)
+    
+    if(batchFlag is True):
+        adata.obs['batch'] = cellInfo['batch'].values
+    
+    sc.pp.filter_cells(adata, min_counts=1)
+
+    if(batchFlag is True):
+        scvi.data.setup_anndata(adata, batch_key = 'batch')
+    else:
+        scvi.data.setup_anndata(adata)
+    model = scvi.model.SCVI(adata, n_latent = nLatent, use_cuda = gpuFlag)
+    
+    model.train()
+
+    if(batchFlag is True):
+        imputeTmp = np.zeros((len(cellSelect), bandM.shape[1]))
+        for batchName in list(set(cellInfo['batch'].values)):
+            imputeTmp = imputeTmp + model.get_normalized_expression(library_size = bandDepth, transform_batch = batchName)
+        imputeM = imputeTmp/len(list(set(cellInfo['batch'].values)))
+
+    else:
+        imputeM = model.get_normalized_expression(library_size = bandDepth)
+        
+    normCount = get_locuspair(imputeM, chromSelect, bandDist)
+
+    latent = model.get_latent_representation()
+    latentDF = pd.DataFrame(latent, index = cellSelect)
+    latentDF = latentDF.reindex([i for i in range(len(bandM))]).fillna(0)
+
+    return(latentDF, normCount)
+
+
+
+def get_args():
+    '''Get arguments'''
+    parser = argparse.ArgumentParser(description = '------------Usage Start------------',
+                                     epilog = '------------Usage End------------')
+    parser.add_argument('-b', '--bandMax', help = 'Maximum genomic distance to be processed, e.g. 10. Use "whole" to include all the band matrix for each chromosome. Default is "whole".', default = 'whole')
+    parser.add_argument('-c', '--chromList', help = 'List of chromosome to be processed separate by comma, e.g. "chr1,chr2,chrX". Use "whole" to include all chromosomes in the cell summary file (args.cellSummary). Default is "whole".', default = 'whole')
+    parser.add_argument('-r', '--resolution', help = 'Resolution of scHi-C data, e.g., 1000000.', default = None)
+    parser.add_argument('-i', '--inPath', help = 'Path to the folder where input scHi-C data are saved.', default = None)
+    parser.add_argument('-o', '--outdir', help = 'Path to output directory.', default = None)
+    parser.add_argument('-cs', '--cellSummary', help = '(Optional) Cell summary file with columns names to be "name" for scHi-C data file name including extension, "batch" for batch factor, "cell_type" for cluster or cell type label (tab separated file).', default = None)
+    parser.add_argument('-g', '--genome', help = 'Path to genome size file (tab separated file).', default = None)
+    parser.add_argument('-br', '--batchRemoval', help = 'Indicator to remove batch or not. Default is False.', action='store_true')
+    parser.add_argument('-n', '--nLatent', help = 'Dimension of latent space. Default is 100.', default = 100)
+    parser.add_argument('-gpu', '--gpuFlag', help = '(Optional) Use GPU or not. Default is False.', action='store_true')
+    parser.add_argument('-p', '--parallelCPU', help = '(Optional) Number of CPUs to be used for parallel running. Default is 1 and no parallel computing is used.', default = 1)
+    parser.add_argument('-pca', '--pcaNum', help = '(Optional) Number of principal components to be writen out. Default is 50.', default = 50)
+    parser.add_argument('-up', '--umapPlot', help = '(Optional) Plot UMAP of latent embeddings. Default is False', action='store_true')
+    parser.add_argument('-tp', '--tsnePlot', help = '(Optional) Plot t-SNE of latent embeddings. Default is False', action='store_true')
+    parser.add_argument('-v', '--verbose', help = '(Optional) Verbose. Default is False.', action='store_true')
+
+    
+    args = parser.parse_args()
+    
+    if args.bandMax != "whole":
+        if int(args.bandMax) <= 0:
+            print("Maximum distance as positive integer for band matrix need to be specified.")
+            parser.print_help()
+            sys.exit()
+    
+    if args.resolution is None:
+        print("Please provide the resolution of the data.")
+        parser.print_help()
+        sys.exit()
+    if args.inPath is None:
+        print("Path to the input scHi-C data need to be specified.")
+        parser.print_help()
+        sys.exit()
+    if args.outdir is None:
+        print("Path to output directory need to be specified.")
+        parser.print_help()
+        sys.exit()
+    else:
+        if not os.path.exists(args.outdir):
+            os.makedirs(args.outdir)
+            
+    if args.batchRemoval is True:
+        if not os.path.exists(args.cellSummary):
+            print("Cell summary file with batch information does not exist.")
+            parser.print_help()
+            sys.exit()
+        cellInfo = pd.read_csv(args.cellSummary, sep = "\t", header = 0)
+        if 'batch' not in cellInfo.columns:
+            print("There is no column in cell summary file called 'batch'.")
+            parser.print_help()
+            sys.exit()
+    
+    if not os.path.exists(args.genome):
+        print("The genome size file does not exist.")
+        parser.print_help()
+        sys.exit()
+        
+    if int(args.nLatent) <= 0:
+        print("The number of dimension of latent space need to be set as nonnegative integer.")
+        parser.print_help()
+        sys.exit()
+        
+    
+    if int(args.parallelCPU) == -1:
+        print("All the CPUs will be used.")
+    if int(args.parallelCPU) == -2:
+        print("All the CPUs but one will be used.")
+    if int(args.parallelCPU) < -2:
+        print("Please provide number of CPUs for parallel computing.")
+        parser.print_help()
+        sys.exit()
+        
+    if int(args.pcaNum) < 2:
+        print("Please provide a number larger than 1 for PCA results writing out.")
+        parser.print_help()
+        sys.exit()
+    return args
+
+if __name__ == "__main__":
+
+    args = get_args()
+    
+    if args.verbose:
+        print('Maximum genomic distance:', args.bandMax)
+        print('Chromosomes to be processed:', args.chromList)
+        print('Resolution:', args.resolution)
+        print('Path to the input scHi-C data:', args.inPath)
+        print('Path to output directory:', args.outdir)
+        print('Cell summary file:', args.cellSummary)
+        print('Genome size file:', args.genome)
+        print('Remove batch effect or not:', args.batchRemoval)
+        print('Dimension of latent space:', args.nLatent)
+        print('Use GPU or not:', args.gpuFlag)
+        print('Number of CPUs for parallel computing:', args.parallelCPU)
+        print('PCA number:', args.pcaNum)
+        print('UMAP plot:', args.umapPlot)
+        print('t-SNE plot:', args.tsnePlot)
+   
+    
+
+    ## number of bin per chromosome
+    print("Caculate total number of bin per chromosome.")
+    binSize = pd.read_csv(args.genome, sep = "\t", header = None)
+
+    binSizeDict = {}
+    N = binSize.shape[0]
+    for i in range(N):
+        chrome = binSize.iloc[i,0]
+        size = binSize.iloc[i,1]
+        binSizeDict[chrome] = size
+    
+    ## cell info file
+    print("Prepare cell summary file.")
+    if args.cellSummary is not None:
+        cellInfo = pd.read_csv(args.cellSummary, sep = "\t", header = 0).sort_values(by = 'name')
+    else:
+        cellName = {'name': os.listdir(args.inPath)}
+        cellInfo = pd.DataFrame(cellName).sort_values(by = 'name')
+
+    cellInfo.index = range(cellInfo.shape[0])
+    
+    ## read in scHi-C data
+    print("Read in scHi-C data.")
+    files = list(args.inPath + '/' + cellInfo.name)
+    files.sort()
+    
+    ## read all the files and sort by cell file name
+    resolution = int(args.resolution)
+    coreN = int(args.parallelCPU)
+    if args.bandMax == "whole":
+        used_diags = "whole"
+    else:
+        used_diags = [i for i in range(1, int(args.bandMax) + 1)]
+    
+    if args.chromList == "whole":
+        used_chroms = "whole"
+    else:
+        used_chroms = args.chromList.split(',')
+    
+    raws = read_files(files, used_chroms, coreN)
+    
+    print("Convert interactions into band matrix.")
+    raw_cells = Parallel(n_jobs=coreN)(delayed(process_cell)(cell_df, binSizeDict, resolution, cell, used_chroms, used_diags) for cell, cell_df in tqdm(raws.groupby('cell')))
+    raw_cells.sort(key=lambda x: x[1]) ##x[1] is 'cell', used for sorting
+    cells = [cell for _, cell in raw_cells]
+    raw_cells = [raw_cell for raw_cell, _ in raw_cells]
+    if not os.path.exists(outdir + '/pickle'):
+        os.mkdir(outdir + '/pickle')
+    with open(outdir + '/pickle/raw_cells', 'wb') as f:
+        pickle.dump(raw_cells, f)
+    # del raws
+    # gc.collect()
+    
+    print("Concat cells into cell x locus-pair matrix.")
+    band_chrom_diag = {}
+    for chrom, chromSize in binSizeDict.items():
+        if used_chroms != "whole" and chrom not in used_chroms:
+            continue
+        chromSize = chromSize // resolution + 1
+        chrom_diag = {}
+        for band in range(1, chromSize):
+            if used_diags != "whole" and band not in used_diags:
+                continue
+            mat = []
+            for fi in range(len(files)):
+                if band not in raw_cells[fi][chrom]:
+                    tmp = np.zeros(chromSize - band)
+                else:
+                    tmp = raw_cells[fi][chrom][band]
+                mat.append(tmp)
+            chrom_diag[band] = np.vstack(mat)
+        band_chrom_diag[chrom] = chrom_diag
+    
+    with open(outdir + '/pickle/band_chrom_diag', 'wb') as f:
+        pickle.dump(band_chrom_diag, f)
+    
+    # del raw_cells
+    # gc.collect()
+
+
+    ## 3DVI
+    ## sys.path.insert(1, '/home/yzheng23/.conda/envs/scvi-env/lib/python3.7/site-packages/')
+    import scvi
+
+    print("3DVI normalization.")
+    bandMiter = [[bandM, chromSelect, bandDist] for chromSelect, band_diags in band_chrom_diag.items() for bandDist, bandM in band_diags.items()]
+    nLatent = int(args.nLatent) #int(args.nLatent)
+    batchFlag = args.batchRemoval
+    gpuFlag = args.gpuFlag
+
+    res = Parallel(n_jobs=coreN,backend='multiprocessing')(delayed(normalize)(bandM, chromSelect, bandDist, nLatent, batchFlag, gpuFlag) for bandM, chromSelect, bandDist in bandMiter)
+    with open(outdir + '/pickle/res', 'wb') as f:
+        pickle.dump(res, f)
+    
+    print("Writing out latent embeddings.")
+    outdir = args.outdir
+    if not os.path.exists(outdir + '/norm3DVI'):
+        os.mkdir(outdir + '/norm3DVI')
+    
+
+    ## remove existing files
+    i = 0
+    for cellId, cellDf in res[i][1].groupby('cellID'):
+            fname = outdir + '/norm3DVI/' + cells[int(cellId)].split('/')[-1]
+            if os.path.exists(fname):
+                os.remove(fname)
+            cellDf.drop(columns=['cellID']).to_csv(fname, sep='\t', header=False, index=False, mode='a')
+    
+    print('Writing out normalization count.')
+    for i in range(1, len(res)):
+        for cellId, cellDf in res[i][1].groupby('cellID'):
+            fname = outdir + '/norm3DVI/' + cells[int(cellId)].split('/')[-1]
+            cellDf.drop(columns=['cellID']).to_csv(fname, sep='\t', header=False, index=False, mode='a')
+    
+    ## concatenate latent embeddings across band matrices
+    latentList = [res[i][0] for i in range(len(res))]
+    latentM = pd.concat(latentList, axis = 1)
+    pca = PCA(n_components = int(args.pcaNum))
+    latentPCA = pca.fit_transform(latentM)
+    
+    if not os.path.exists(outdir + '/latentEmbeddings'):
+        os.mkdir(outdir + '/latentEmbeddings')
+    pd.DataFrame(latentPCA).to_csv(outdir + '/latentEmbeddings/norm3DVI_PCA' + str(args.pcaNum) + '.txt', sep = '\t', header = False, index = False)
+    pd.DataFrame(latentM).to_csv(outdir + '/latentEmbeddings/norm3DVI_latentEmbeddingFull.txt', sep = '\t', header = False, index = False)
+    
+    
+    ## Visualization
+    if args.umapPlot:
+        print("UMAP visualization.")
+        if not os.path.exists(outdir + '/figures/'):
+            os.mkdir(outdir + '/figures/')
+        import matplotlib.pyplot as plt
+        import umap
+        reducer = umap.UMAP()
+        latentUMAP = reducer.fit_transform(latentPCA)
+
+        colorDict = dict(zip(set(cellInfo.cell_type), range(len(set(cellInfo.cell_type)))))
+        cellLabel = [colorDict[cellInfo['cell_type'][i]] for i in range(len(cellInfo))]
+ 
+        fig, ax = plt.subplots(1, figsize=(14, 10))
+        plt.scatter(
+            latentUMAP[:, 0],
+            latentUMAP[:, 1],
+            c = cellLabel)
+        plt.setp(ax, xticks=[], yticks=[])
+        cbar = plt.colorbar(boundaries=np.arange(len(list(colorDict.keys()))+1) - 0.5)
+        cbar.set_ticks(np.arange(len(list(colorDict.keys()))+2))
+        cbar.set_ticklabels(list(colorDict.keys()))
+        plt.title('UMAP Projection of the scHi-C Demo Data', fontsize=15)
+        plt.savefig(outdir + '/figures/norm3DVI_UMAP.pdf')  
+
+    if args.tsnePlot:
+        print("t-SNE visualization.")
+        if not os.path.exists(outdir + '/figures/'):
+            os.mkdir(outdir + '/figures/')
+        import matplotlib.pyplot as plt
+        from sklearn.manifold import TSNE
+        tsne = TSNE(n_components=2, random_state=0)
+        latentTSNE = tsne.fit_transform(latentPCA)
+
+        fig, ax = plt.subplots(1, figsize=(14, 10))
+        plt.scatter(
+            latentTSNE[:, 0],
+            latentTSNE[:, 1],
+            c = cellLabel)
+        plt.setp(ax, xticks=[], yticks=[])
+        cbar = plt.colorbar(boundaries=np.arange(len(list(colorDict.keys()))+1) - 0.5)
+        cbar.set_ticks(np.arange(len(list(colorDict.keys()))+2))
+        cbar.set_ticklabels(list(colorDict.keys()))
+        plt.title('t-SNE Projection of the scHi-C Demo Data', fontsize=15)
+        plt.savefig(outdir + '/figures/norm3DVI_TSNE.pdf')  
